8e22236040ed0e2391cb17aab202dd59953e54b1
ArikSquad <business.ariksquad@gmail.com>
2024-10-09 12:04:53 +0300

    perf: Remove unused packages

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,3 @@
-import { Icons } from "@/components/icons"
-
 export type NavItem = {
     title: string
     href: string
@@ -7,44 +5,3 @@ export type NavItem = {
 }
 
 export type MainNavItem = NavItem
-
-export type SidebarNavItem = {
-    title: string
-    disabled?: boolean
-    external?: boolean
-    icon?: keyof typeof Icons
-} & (
-    | {
-    href: string
-    items?: never
-}
-    | {
-    href?: string
-    items: NavLink[]
-}
-    )
-
-export type SiteConfig = {
-    name: string
-    description: string
-    url: string
-    ogImage: string
-    links: {
-        twitter: string
-        github: string
-    }
-}
-
-export type DocsConfig = {
-    mainNav: MainNavItem[]
-    sidebarNav: SidebarNavItem[]
-}
-
-export type MarketingConfig = {
-    mainNav: MainNavItem[]
-}
-
-export type DashboardConfig = {
-    mainNav: MainNavItem[]
-    sidebarNav: SidebarNavItem[]
-}
