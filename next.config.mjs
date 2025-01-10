a9083f3e29f24b1e1063124070ea01d21255810d
AriDev <75741608+ArikSquad@users.noreply.github.com>
2025-01-11 01:07:17 +0200

    fix: update discord link

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,7 @@ export default withNextra({
             destination: '/docs/:slug*',
             permanent: true,
         },
-        {source: '/flow/discord', destination: 'https://discord.gg/VUAHvffhTz', permanent: true},
+        {source: '/flow/discord', destination: 'https://discord.gg/SuXGbq24wA', permanent: true},
         {source: '/flow/earth', destination: 'https://earth.mikart.eu/', permanent: true},
         {source: '/flow/store', destination: 'https://store.mikart.eu/', permanent: true},
     ],
