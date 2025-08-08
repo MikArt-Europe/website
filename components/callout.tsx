9cc2e1c6a1d2b64cb7fd09b7633b1824ae2e74ae
AriDev <75741608+ArikSquad@users.noreply.github.com>
2025-08-09 01:07:34 +0300

    fix: callout uses tailwind-merge wrong

--- components/callout.tsx
+++ components/callout.tsx
@@ -10,10 +10,11 @@ interface CalloutProps {
 export function Callout({ children, icon, type = 'default', ...props }: CalloutProps) {
     return (
         <div
-            className={cn('my-6 flex items-start rounded-md border border-l-4 p-4', {
-                'border-red-900': type === 'danger',
-                'border-yellow-900': type === 'warning'
-            })}
+            className={cn(
+                'my-6 flex items-start rounded-md border border-l-4 p-4',
+                type === 'danger' && 'border-red-900',
+                type === 'warning' && 'border-yellow-900'
+            )}
             {...props}
         >
             {icon && <span className="mr-4 text-2xl">{icon}</span>}
