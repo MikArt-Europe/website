e4750bc0ff744f2bd87a3b9ef09b21bdb633aea4
AriDev <75741608+ArikSquad@users.noreply.github.com>
2025-08-09 01:40:05 +0300

    fix: bad import

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,12 @@
 'use client'
 
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ * This configuration is used to for the Sanity Studio mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structure as structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
